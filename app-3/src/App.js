c0fa2013fd19fcdf0a6bd83d4791ab46e0f1abcf
Nathan Paddock <nathanpaddock@Nathans-MacBook-Pro.local>
2018-10-01 15:45:55 -0700

    App3 Completed

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      foodString: "",
+      foods: [
+        "spaghetti",
+        "ice cream",
+        "sushi",
+        "bologna",
+        "cheese"
+      ]
+    }
+
+  }
+
+  handleChange(event){
+    this.setState({foodString: event})
+  }
+  
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    let foodsToDisplay = this.state.foods.filter( (element, index)=>{
+      return element.includes(this.state.foodString);
+    }).map( (element, index)=>{
+      return <h2 key={index}>{element}</h2>
+    })
+
+    return(
+    <div className='App'>
+      <input onChange={(e)=>this.handleChange(e.target.value)} type="text"/>
+      {foodsToDisplay}
       </div>
     );
   }
