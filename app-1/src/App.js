def09e29d89336a244b51c127c2857c2330df230
Nathan Paddock <nathanpaddock@Nathans-MacBook-Pro.local>
2018-10-01 14:15:39 -0700

    App 1 Completed

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -3,18 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  constructor () {
+    super();
+
+      this.state = {
+        message: "hello"
+      }  
+  }
+
+forChanges(val){
+  this.setState({message: val})
+}
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className = 'App'>
+        <input onChange = {event=>this.forChanges(event.target.value)}></input>
+          <p>{this.state.message}</p>
       </div>
-    );
+    )
   }
 }
 
